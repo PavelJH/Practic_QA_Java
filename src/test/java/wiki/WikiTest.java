a0c56912195eae98969924b2c0a80cbc5458fd29
PavelJH <kelbaspavel@gmail.com>
2025-02-23 11:49:27 +0100

    Ended WiKi tests

--- src/test/java/wiki/WikiTest.java
+++ src/test/java/wiki/WikiTest.java
@@ -4,11 +4,16 @@ package wiki;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 import core.BaseTest;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Selenide.$$x;
 
@@ -57,6 +62,53 @@ public class WikiTest extends BaseTest {
         /**
          * Получаем значение из полученных ссылок
          */
-        links.forEach(Selenide::open);//Открытие всех полученных ссылок через StreamApi
+        links.forEach(Selenide::open);//Открытие всех полученных ссылок через StreamApi и сравнения ссылки которую нам достоится из коллекции
+    }
+    @Test
+    public void openAllHrefsWithStreaAPIFOR() {
+        Selenide.open(URL); // Открытие браузера
+        ElementsCollection hrefs = $$x("//div[@id='toc']//a[@href]");
+        List<String> links = new ArrayList<>();
+        hrefs.forEach(x->links.add(x.getAttribute("href")));
+        for (int i = 0; i< links.size(); i++){
+            String listUrl = links.get(i);//получения внешний ссылки, с которой можем работать, из списка
+            Selenide.open(listUrl);//открываем ссылку которую достали в предыдущем шаге
+            String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();//ссылка которая достоится из адресной строки, ссылка из Браузера
+            Assert.assertEquals(currentUrl, listUrl);// Сравниваем ссылку из списка и сравниваем ее с поточной, которая открывается в браузере
+        }
+        }
+    /**
+     * Получаем случайное значение из списка
+     * Достаем какие-то значения
+     * И в открывшейся ссылке, убираем со списка
+     * Тоесть в рандомном порядке открыть все ссылки
+     */
+    @Test
+    public void openAllHrefsWithStreaAPIRandomValue(){
+        Selenide.open(URL); // Открытие браузера
+        ElementsCollection hrefs = $$x("//div[@id='toc']//a[@href]");
+        List<String> links = new ArrayList<>();
+        hrefs.forEach(x->links.add(x.getAttribute("href")));
+        //Random random = new Random(); // Класс для вычисления каких-то рандомных значений// пересеяно чуть ниже
+        while (links.size() > 0) { // Пока размер списка будет больше нуля, будем выполнять какие-то вычесления
+            int randomNumber = new Random().nextInt(links.size());// Перенесено сюда, так как мы используем данную переменную лишь один раз, здесь
+            Selenide.open(links.get(randomNumber));// открываем надомную ссылку
+            links.remove(WebDriverRunner.getWebDriver().getCurrentUrl());//получаем значения открытой ссылки и удаляем ее из списка
+        }
+    }
+
+    /**
+     * Получаем длину ссылок
+     * В итоге получаем список с числами
+     * map - промежуточный метод, который конвертирует один тип данных в другой
+     */
+    @Test
+    public void openAllHrefsWithFullStreaAPI(){
+        Selenide.open(URL); // Открытие браузера
+        ElementsCollection hrefs = $$x("//div[@id='toc']//a[@href]");
+        List<String> links = new ArrayList<>();
+        hrefs.forEach(x->links.add(x.getAttribute("href")));
+        List<Integer> linksLenghth = hrefs.stream().map(x->x.getAttribute("href").length()).toList();// В конце засовываем все полученные данные в лист
+    int i = 0;
     }
 }
